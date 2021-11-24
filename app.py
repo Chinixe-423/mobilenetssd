8b5ac2d3acf257cf6a01e8ef03e933695a913f7c
Chinixe-423 <89176794+Chinixe-423@users.noreply.github.com>
2021-11-24 14:35:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def callback():
     no_event = len(decoded['events'])
     for i in range(no_event):
             event = decoded['events'][i]
-            event_handle(event,jason_line)
+            event_handle(event,json_line)
 
     # เชื่อมต่อกับ dialogflow
     #intent = decoded["queryResult"]["intent"]["displayName"] 
@@ -125,7 +125,7 @@ def reply(intent,text,reply_token,id,disname):
     text_message = TextSendMessage(text="ทดสอบ")
     line_bot_api.reply_message(reply_token,text_message)
 
-def event_handle(event,jason_line):
+def event_handle(event,json_line):
     print(event)
     try:
         userId = event['source']['userId']
@@ -160,11 +160,11 @@ def event_handle(event,jason_line):
             replyObj = TextSendMessage(text="USA กับ JAPANหน่ะ")
             line_bot_api.reply_message(rtoken, replyObj)
         else : 
-          eaders = request.headers
-          json_headers = ({k:v for k, v in headers.items()})
-          json_headers.update({'Host':'bots.dialogflow.com'})
-          url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/68745306-8684-4f87-a9d0-c375b348faea"
-          requests.post(url,data=json_line, headers=json_headers)
+            eaders = request.headers
+            json_headers = ({k:v for k, v in headers.items()})
+            json_headers.update({'Host':'bots.dialogflow.com'})
+            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/68745306-8684-4f87-a9d0-c375b348faea"
+            requests.post(url,data=json_line, headers=json_headers)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
